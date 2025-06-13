65910eccb95a77e2d8d76b145c6eb1bdd8f8b2b9
m_vianello <manuel.vianello94@gmail.com>
2025-06-13 17:42:24 +0200

    Add iceberg-aws-bundle entrypoint.sh

--- spark-stream-processor/entrypoint.sh
+++ spark-stream-processor/entrypoint.sh
@@ -3,7 +3,7 @@
 SPARK_VERSION="${SPARK_VERSION:-3.5.1}"
 SPARK_VERSION_SHORT=$(echo "$SPARK_VERSION" | cut -d '.' -f1,2)
 SCALA_VERSION="${SCALA_VERSION:-2.12}"
-ICEBERG_VERSION="${ICEBERG_VERSION:-1.4.3}"
+ICEBERG_VERSION="${ICEBERG_VERSION:-1.9.1}"
 NESSIE_VERSION="${NESSIE_VERSION:-0.104.1}"
 
 echo "🚀 Starting Spark job with:"
@@ -13,8 +13,10 @@ echo "    Iceberg version: ${ICEBERG_VERSION}"
 echo "    Nessie version: ${NESSIE_VERSION}"
 
 spark-submit \
-  --packages org.apache.iceberg:iceberg-spark-runtime-${SPARK_VERSION_SHORT}_${SCALA_VERSION}:${ICEBERG_VERSION},org.apache.spark:spark-sql-kafka-0-10_${SCALA_VERSION}:${SPARK_VERSION},org.projectnessie.nessie-integrations:nessie-spark-extensions-${SPARK_VERSION_SHORT}_${SCALA_VERSION}:${NESSIE_VERSION} \
+  --packages org.apache.iceberg:iceberg-spark-runtime-${SPARK_VERSION_SHORT}_${SCALA_VERSION}:${ICEBERG_VERSION},org.apache.iceberg:iceberg-aws-bundle:${ICEBERG_VERSION},org.apache.spark:spark-sql-kafka-0-10_${SCALA_VERSION}:${SPARK_VERSION} \
   --conf "spark.driver.extraJavaOptions=-Divy.cache.dir=/tmp -Divy.home=/tmp" \
   --conf spark.hadoop.fs.s3a.connection.timeout=60000 \
   /app/code/main.py
 
+
+# --packages org.apache.iceberg:iceberg-spark-runtime-${SPARK_VERSION_SHORT}_${SCALA_VERSION}:${ICEBERG_VERSION},org.apache.spark:spark-sql-kafka-0-10_${SCALA_VERSION}:${SPARK_VERSION},org.projectnessie.nessie-integrations:nessie-spark-extensions-${SPARK_VERSION_SHORT}_${SCALA_VERSION}:${NESSIE_VERSION} \
